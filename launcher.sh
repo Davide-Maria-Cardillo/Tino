94400ac9f97e8a5474f91179d86a307f79130b27
Davide-Maria-Cardillo <64076941+Davide-Maria-Cardillo@users.noreply.github.com>
2024-05-13 10:59:13 +0200

    Add files via upload

---
+++ launcher.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+# start new MAIN.PY
+cd "$(dirname "$(readlink -f "$0")")"
+
+# Activate the virtual environment
+source ~/.venv/eternia/bin/activate
+
+# Execute the Python script
+python3 main.py
---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+import multiprocessing
+from classes.control import Control
